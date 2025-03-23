f7df8dd83691a17e601b58fb24afb8df5556e992
svinstvo-og <maxim12.05.2006koval@gmail.com>
2025-03-24 00:57:48 +0100

    first commit

---
+++ src/test/java/seine/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package seine.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
